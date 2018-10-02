f59fc208d4d5df6bfdf87547ae2fd637900f6036
rmorgado <!-- Top Banner  --	 aside id="top-banner" class="container"	 	div data-js-ad-manager-slot="div-hola-slot-megabanner"/div /aside>
2018-10-02 14:46:50 +0200

    añadido componente de scnack bar

---
+++ src/components/SnackBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-snackbar
+        :timeout="timeout"
+        :color="color"
+        v-model="showSnack"
+    >
+        {{ text }}
+        <v-btn dark flat @click="showSnack = false">
+            {{ $t('message.close') }}
+        </v-btn>
+    </v-snackbar>
+</template>
+<script>
+    export default {
+        name: 'app-snack-bar',
+        props: ['snackbar', 'timeout', 'text'],
+        data () {
+            return {
+                color: 'primary',
+                mode: 'horizontal',
+                showSnack: this.snackBar
+            }
+        }
+    }
+</script>
